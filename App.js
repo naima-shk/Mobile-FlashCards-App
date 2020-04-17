9fdc0983e3c88dd68fd25d26c520b52d0455062e
naima shaikh <naimashaikh550@gmail.com>
2020-04-17 22:14:11 +0500

    'Final-project'

--- App.js
+++ App.js
@@ -1,114 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { receiveDecks } from "./actions/index";
+import * as Font from "expo-font";
+import { robotoMedium, robotoRegular } from "./utils/fonts";
+import NavigationService from "./navigation/navigationService";
+import { fetchAllDecks } from "./utils/api";
+import { setLocalNotification } from "./utils/notificationHelper";
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import StackNavigation from "./navigation/StackNavigation";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  store = createStore(reducer, middleware);
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
+  state = {
+    prerequisitesLoaded: false, // Flags if the Roboto fonts and data have been loaded
+  };
+
+  async componentDidMount() {
+    await setLocalNotification();
+
+    const loadDecksPromise = fetchAllDecks();
+
+    const loadFontsPromise = Font.loadAsync({
+      [robotoRegular]: require("./assets/fonts/Roboto-Regular.ttf"),
+      [robotoMedium]: require("./assets/fonts/Roboto-Medium.ttf"),
+    });
+
+    Promise.all([loadDecksPromise, loadFontsPromise]).then((values) => {
+      const decks = values[0];
+      this.store.dispatch(receiveDecks(decks));
+
+      this.setState({
+        prerequisitesLoaded: true,
+      });
+    });
+  }
+
+  // --------------------------------------------------------------------------------
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <View style={styles.appContainer}>
+          {this.state.prerequisitesLoaded && (
+            <StackNavigation
+              ref={(navigatorRef) => {
+                NavigationService.setTopLevelNavigator(navigatorRef);
+              }}
+            />
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        </View>
+      </Provider>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  appContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
   },
 });
-
-export default App;
